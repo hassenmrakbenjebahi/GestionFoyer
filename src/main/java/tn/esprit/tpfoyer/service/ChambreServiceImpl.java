bd92937b72ec2aef364f9c07f818da045eb6d38b
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2023-11-28 12:46:09 +0100

    schudles+springAOP

--- src/main/java/tn/esprit/tpfoyer/service/ChambreServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/ChambreServiceImpl.java
@@ -1,6 +1,8 @@
 package tn.esprit.tpfoyer.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import tn.esprit.tpfoyer.entity.Chambre;
 import tn.esprit.tpfoyer.repository.ChambreRepository;
@@ -9,10 +11,16 @@ import tn.esprit.tpfoyer.service.IChambreService;
 import java.util.List;
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ChambreServiceImpl implements IChambreService {
     ChambreRepository chambreRepository;
+    @Scheduled(fixedDelay = 10000) // 10000 millisecondes
     public List<Chambre> retrieveAllChambres() {
-        return chambreRepository.findAll();
+        List<Chambre> listC=chambreRepository.findAll();
+        for (Chambre c: listC) {
+            log.info("Chambre :" + c);
+        }
+        return listC;
     }
     public Chambre retrieveChambre(Long chambreId) {
         return chambreRepository.findById(chambreId).get();
